cbfec24e02fcd2f7c0e1787fb7ebf46792e3682c
dongdongdongha <109192056+dongdongdongha@users.noreply.github.com>
2022-07-13 12:28:37 +0900

    1st.
    첫

---
+++ 반복문1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+	int n = 0;
+	
+	while(n<5){
+		printf("%d", ++n);
+		//n++;
+	} 
+	printf("\n루프종료\n");
+	
+	
+	
+	int i = 30;
+	while(i >= 10){
+		printf("%d ", i);
+		i = i - 2;
+	}
+}
